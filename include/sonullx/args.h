587204f1ed4e5a917e3d5d8fbfbb95eb8bb94b26
Sonullx Liu <Sonullx.Liu@gmail.com>
2020-12-18 14:50:47 +0800

    build: add basic files

---
+++ include/sonullx/args.h
@@ -0,0 +1,12 @@
+//
+// Created by Sonullx Liu on 2020-12-18.
+//
+
+#ifndef ARGS_ARGS_H
+#define ARGS_ARGS_H
+
+namespace sonullx
+{
+}
+
+#endif //ARGS_ARGS_H
---
+++ src/test.cpp
@@ -0,0 +1,10 @@
+//
+// Created by Sonullx Liu on 2020-12-18.
+//
+
+#include <sonullx/args.h>
+
+int main(int argc, char ** argv)
+{
+	return 0;
+}
